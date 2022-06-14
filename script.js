821da542d9fd42190949ea0b39b9b37291b3926d
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-06-13 21:25:51 -0300

    build: Construindo algumas funções

--- script.js
+++ script.js
@@ -2,18 +2,21 @@
 
 // forEach
 function newForEach(array, callback) {
+  let newArray = [];
   for (let i = 0; i < array.length; i++) {
-    callback(array[i], i, array);
+    newArray.push(callback(array[i], i, array));
   }
+  return newArray;
 }
 
 const arrayForEach = ["<", "<", "<"];
 const arrayForEachTwo = [";", ";", ";"];
 
 function testForEach(elem, i, array) {
-  console.log((array[i] = elem + "D"));
+  return (array[i] = elem + 3);
 }
 
+// console.log(newForEach(arrayForEach, testForEach));
 // console.log(newForEach(arrayForEachTwo, testForEach));
 
 // map
@@ -148,9 +151,152 @@ function newReduce(array, callback) {
 }
 
 const arrayReduce = [50, 50];
+const arrayReduceTwo = [500, 250];
 
 function reduceTest(acc, elem, index, array) {
   return acc + elem;
 }
 
-console.log(newReduce(arrayReduce, reduceTest));
+function reduceTestTwo(acc, elem, index, array) {
+  return acc - elem;
+}
+
+// console.log(newReduce(arrayReduce, reduceTest));
+// console.log(newReduce(arrayReduceTwo, reduceTestTwo));
+
+// findIndex
+function newFindIndex(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+const arrayFindIndex = [
+  "Futebol",
+  "Ping-pong",
+  "Vôlei",
+  "Frescobol",
+  "Basquete",
+];
+
+function findIndexTest(elem, index, array) {
+  return elem == "Basquete";
+}
+
+// console.log(newFindIndex(arrayFindIndex, findIndexTest));
+
+//  some
+function newSome(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const arraySome = [2, 5, 8, 1, 4];
+const arraySomeTwo = [12, 5, 8, 1, 4];
+
+function someTest(elem, index, array) {
+  return elem > 10;
+}
+
+// console.log(newSome(arraySome, someTest));
+// console.log(newSome(arraySomeTwo, someTest));
+
+// every
+function newEvery(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (!callback(array[i], i, array)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const arrayEvery = [12, 5, 8, 130, 44];
+const arrayEveryTwo = [12, 54, 18, 130, 44];
+
+function everyTest(elem, index, array) {
+  return elem >= 10;
+}
+
+// console.log(newEvery(arrayEvery, everyTest));
+// console.log(newEvery(arrayEveryTwo, everyTest));
+
+// concat
+function newConcat(array, args) {
+  const newArray = array.slice();
+  for (let i = 0; i < args.length; i++) {
+    newArray.push(args[i]);
+  }
+  return newArray;
+}
+
+const arrayConcat = ["a", "b", "c"];
+const arrayConcatTwo = ["d", "e", "f"];
+
+// console.log(newConcat(arrayConcat, arrayConcatTwo));
+
+// join
+function newJoin(array, separador) {
+  let newString = "";
+  for (let i = 0; i < array.length; i++) {
+    newString += array[i];
+    if (i < array.length - 1) {
+      newString += separador;
+    }
+  }
+  return newString;
+}
+
+const arrayJoin = ["Fire", "Air", "Water"];
+
+// console.log(newJoin(arrayJoin, ''));
+
+// slice
+function newSlice(array, inicio, fim) {
+  const newArray = [];
+  for (let i = inicio; i < fim; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+
+const arraySlice = ["Banana", "Laranja", "Limao", "Maçã", "Manga"];
+
+// console.log(newSlice(arraySlice, 1, 3));
+
+// flat
+// function newFlat(array) {
+//   const newArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     newArray.push(array[i], i, array);
+//   }
+//   return newArray;
+// }
+
+// const arrayFlat = [1, 2, [3, 4]];
+
+// console.log(newFlat(arrayFlat));
+
+// flatMap
+function newFlatMap(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callback(array[i], i, array));
+  }
+  return newArray;
+}
+
+const arrayFlatMap = [1, 2, 3, 4, 5];
+
+function flatMapTest(elem, index, array) {
+  return [elem * 2];
+}
+
+// console.log(newFlatMap(arrayFlatMap, flatMapTest));
